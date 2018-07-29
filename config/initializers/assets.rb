0cb561665acfa8529b75422762938ef6fd2d2036
shu.853 <shu.esu853@gmail.com>
2018-07-29 05:50:29 +0000

    adjust layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 ## vendor/javascript 読み込み用 
-Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( *.js *.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
